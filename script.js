cdad2d9e171660adb50aed35d7c540c9d19ab868
aqsa <aqsamalikurl@gmail.com>
2025-05-17 22:00:16 +0500

    Aqsa work

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Thank you! We will get back to you soon.');
+});
+
+console.log("Landing page script loaded");
